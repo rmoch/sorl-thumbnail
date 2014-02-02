f0c7158fb3df221823201e78fd4b7ea194eae60a
Richard Moch <richard.moch@gmail.com>
2014-02-02 12:42:55 +0100

    keep django <1.4 compatibility in tests

--- tests/thumbnail_tests/urls.py
+++ tests/thumbnail_tests/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, url, include
+# -*- coding: utf-8 -*-
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url  # keep django <1.4 compatibility
 from django.conf import settings
 from django.views.generic import TemplateView
 
